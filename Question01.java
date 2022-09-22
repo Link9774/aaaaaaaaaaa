19dda43b013a26c43b6a0f7c4b88fdc0af2b98f9
Link9774 <111504681+Link9774@users.noreply.github.com>
2022-09-22 08:33:22 -0300

    Add files via upload

---
+++ Question01.java
@@ -0,0 +1,18 @@
+///package br.unicap.p1.atividade02;
+import java.util.Scanner;
+
+public class Question01 {
+
+	public static void main(final String[] args) {
+	    Scanner s = new Scanner(System.in);
+        int N1, N2, N3;
+        N1 = s.nextInt(); 
+        N2 = s.nextInt();
+        N3 = s.nextInt();
+        
+        if (N1>=N2>=N3) {
+
+        }
+
+	}
+}
